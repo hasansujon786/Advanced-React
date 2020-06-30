b134b7dbbd5c432c6e5b4a913a35274b9d9499a6
Hasan Mahmud <hasansujon786@gmail.com>
2020-06-30 13:53:26 +0600

    Create first Item Mutation & Query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if the are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args.data
+      }
+    }, info)
+
+    return item
+  },
+
+};
+
+export default Mutation
+
+
